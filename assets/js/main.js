c801691f3ac1f9d987e6f4a4224918f0d5938f58
Samuel Ryc <samuelryc@Samuels-MacBook-Pro.local>
2020-05-27 08:31:47 -0400

    new isInViewport function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 (function($) {
     const NAV_CLICK_SCROLL_SPEED = 1; // in seconds
-    const SECTION_ONSCROLL_TOP_PADDING = 30; // in pixels
+    const ONSCROLL_SECTION_TOP_PADDING = 30; // in pixels
 
     // List of images for every section
     var image_list = {
@@ -9,11 +9,6 @@
         'tree-3': 'images/tree3.jpg',
         'contact': 'images/tree4.jpg'
     }
-
-    // Helper function that checks if the section is currently seen by the web site visitor
-    $.fn.isInViewport = function() {
-        return $(this).offset().top + $(this).outerHeight() > 100 && $(this).offset().top < $(window).scrollTop() + 550;
-    };
 
     // On scroll, check if a new section is seen
     $('.scrollable-content').on('scroll', function() {
@@ -29,10 +24,19 @@
 
     // On nav click animation to section
     $('.nav-link').click(function(event) {
-        if ($('.sticky-top').length) {
-            $('.scrollable-content').animate({ scrollTop: $($(this).attr('href')).offset().top - $('.scrollable-content').offset().top + $('.scrollable-content').scrollTop() - $('.sticky-top').outerHeight() - SECTION_ONSCROLL_TOP_PADDING }, NAV_CLICK_SCROLL_SPEED * 1000);
+        if ($('.sticky-top').length) { // if the sticky top header is present, take it into consideration before scrolling to element
+            $('.scrollable-content').animate({ scrollTop: $($(this).attr('href')).offset().top - $('.scrollable-content').offset().top + $('.scrollable-content').scrollTop() - $('.sticky-top').outerHeight() - ONSCROLL_SECTION_TOP_PADDING }, NAV_CLICK_SCROLL_SPEED * 1000);
         } else {
-            $('.scrollable-content').animate({ scrollTop: $($(this).attr('href')).offset().top - $('.scrollable-content').offset().top + $('.scrollable-content').scrollTop() }, SECTION_ONSCROLL_TOP_PADDING * 1000);
+            $('.scrollable-content').animate({ scrollTop: $($(this).attr('href')).offset().top - $('.scrollable-content').offset().top + $('.scrollable-content').scrollTop() }, ONSCROLL_SECTION_TOP_PADDING * 1000);
         }
     });
+
+    // Helper function that checks if the section is currently seen by the web site visitor
+    $.fn.isInViewport = function() {
+        if ((($(this).offset().top + $(this).outerHeight()) >= $(window).scrollTop()) && ($(this).offset().top <= ($(window).scrollTop() + $(window).height()))) {
+            return true;
+        }
+
+        return false;
+    }
 })(jQuery);
